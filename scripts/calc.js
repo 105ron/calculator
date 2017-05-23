c82038a6a4c5cc270c0606eafdefc856f7a6c9af
Rhys <rbrooker105@gmail.com>
2017-05-23 21:32:03 +1000

    add sitch statement to make AC and CE buttons work, adjust display with button presses

--- scripts/calc.js
+++ scripts/calc.js
@@ -9,10 +9,34 @@ var myFunction = function() {
   console.log('poo');
   return true;
 }*/
-
+let buttonClick = "";
+let displayString = "";
+let screen = document.getElementById("screen")
 buttons.addEventListener("click", function(event) {
-  let button_click = event.target.innerText;
-  });
+  buttonClick = event.target.innerText;
+  updateString(buttonClick);
+});
+
+function updateDisplay() {
+  //Display the string unless it is "", then display "0"
+  screen.innerText = (displayString ? displayString : "0");
+}
+
+function updateString(click) {
+  switch(click) {
+    case "ac" :
+      displayString = ""
+      updateDisplay();
+      break;
+    case "ce" :
+      displayString = displayString.slice(0, -1);
+      updateDisplay();
+      break;
+    default :
+      displayString += click;
+      updateDisplay();
+  }
+}
 
 var add = function(firstNumber, secondNumber) {
   return (firstNumber + secondNumber)
